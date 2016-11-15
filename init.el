bf07230646249b892d63380b4c5633a5ddec7891
chenshiujin <785795635@qq.com>
2016-11-15 15:29:50 +0800

    trim end whitespace

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
 
 (package-initialize)
 (setq url-http-attempt-keepalives nil)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-hook 'before-save-hook 'gofmt-before-save)
 (defun set-exec-path-from-shell-PATH ()
@@ -40,6 +41,7 @@
     (setenv "PATH" path-from-shell)
     (setq eshell-path-env path-from-shell) ; for eshell users
     (setq exec-path (split-string path-from-shell path-separator))))
+;;(setenv "GOPATH" (shell-command-to-string "pwd"))
 
 (when window-system (set-exec-path-from-shell-PATH))
 ;(when (memq window-system '(mac ns))
@@ -53,9 +55,10 @@
 (defun go-mode-godef-hook()
   (local-set-key (kbd "M-.") 'godef-jump))
 (add-hook 'go-mode-hook 'go-mode-godef-hook)
-(setq exec-path (cons "/usr/bin" exec-path))
-(add-to-list 'exec-path "/usr/bin")
+;(setq exec-path (cons "/usr/bin" exec-path))
+;(add-to-list 'exec-path "/usr/bin")
 (add-hook 'before-save-hook 'gofmt-before-save)
+
 ;; go config end
 
 ;; code solve chinese show problem
@@ -95,3 +98,4 @@
 
 (put 'upcase-region 'disabled nil)
 
+(put 'scroll-left 'disabled nil)
